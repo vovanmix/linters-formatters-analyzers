d78e24dbf1a4b99f369247af307932789347464b
Vladimir Mikhaylovskiy <vlad@indiegogo.com>
2017-05-02 12:57:23 -0700

    rubocop indentation

--- ruby/example.rb
+++ ruby/example.rb
@@ -5,29 +5,39 @@ module Section
 
       hash = {
                a: 'adsadas',
-               b: 'sdfdsf' }
+               b: 'sdfdsf'
+             }
 
       small_hash = {
-                     a: 'adsadas' }
+                     a: 'adsadas'
+                   }
 
       puts "hello #{name}"
 
       small_hash.try(:sda).try(:dfsfd).merge(hash)
-                .each { |key, value| key + value }
-                .each { |key, value| key + value }
+        .each { |key, value| key + value }
+        .each { |key, value| key + value }
 
       two('some-name', opt1: 1, opt2: 'two', opt3: [1, 2, 3])
 
       array = [
-                :value]
+                :value
+              ]
       but_in_a_method_call([
-                             :its_like_this])
+                             :its_like_this
+                           ])
 
       foo_bar(apple,
               banana)
       foo_bar(
         apple,
-        banana)
+        banana
+      )
+
+      myvariable = Thing
+                     .a
+                     .b
+                     .c
     end
 
     def two(name, options, a, b, c)
